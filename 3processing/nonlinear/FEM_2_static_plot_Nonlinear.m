2f22c89d0be49e19dbc433e463cd7c16b0504365
chungntu <66866309+chungntu@users.noreply.github.com>
2023-05-24 11:24:56 +0700

    Delete 3processing directory

--- 3processing/nonlinear/FEM_2_static_plot_Nonlinear.m
+++
@@ -1,6 +0,0 @@
-%--------------------------------------------------------------------------
-% PLOT STATIC ANALYSIS
-% Truong Thanh Chung. Aug 2021.
-%--------------------------------------------------------------------------
-function Np = FEM_2_static_plot_Nonlinear(in_data,resp,opt)
-[Np,~,~] = FEM_3_static_plot_NVM(in_data,resp,opt);
